24044ed3ec6db76fbffa4b722611d9f03b4bef93
Lawrence Job <lawrence@gridfusions.com>
2012-08-05 23:51:10 +0100

    added gosquared

--- delegate.php
+++ delegate.php
@@ -348,7 +348,18 @@ background-position:20px -1px}
 			$('.carousel').carousel()
 			//$('#yrs-info-tabs a:first').tab('show');
         </script>
-
+ <script type="text/javascript">
+  var GoSquared={};
+  GoSquared.acct = "GSN-083699-T";
+  (function(w){
+    function gs(){
+      w._gstc_lt=+(new Date); var d=document;
+      var g = d.createElement("script"); g.type = "text/javascript"; g.async = true; g.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
+      var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(g, s);
+    }
+    w.addEventListener?w.addEventListener("load",gs,false):w.attachEvent("onload",gs);
+  })(window);
+</script>
 
   </body>
 </html>
